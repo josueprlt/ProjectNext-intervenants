f78e5e94cf63ea1c13766617c0a5fbf4d12bcb3f
josueprlt <josue.perrault@etu.unilim.fr>
2024-11-21 15:41:13 +0100

    update

--- src/app/lib/fetchIntervenants.ts
+++ src/app/lib/fetchIntervenants.ts
@@ -1,13 +1,13 @@
 export async function fetchIntervenants() {
-    try {
-      const response = await fetch('/api/users');
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      const data = await response.json();
-      return data;
-    } catch (error) {
-      console.error('Error fetching users:', error);
-      throw error;
+  try {
+    const response = await fetch('/api/users');
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
     }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error fetching users:', error);
+    throw error;
   }
+}
