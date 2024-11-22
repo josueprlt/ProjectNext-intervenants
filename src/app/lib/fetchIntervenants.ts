bb811de3c341fd3bfccf5cfbe0612ae6cf40ec61
josueprlt <josue.perrault@etu.unilim.fr>
2024-11-22 10:09:15 +0100

    up

--- src/app/lib/fetchIntervenants.ts
+++ src/app/lib/fetchIntervenants.ts
@@ -1,6 +1,6 @@
 export async function fetchIntervenants() {
   try {
-    const response = await fetch('');
+    const response = await fetch('SELECT (firstname, name) FROM interventants;');
     if (!response.ok) {
       throw new Error('Network response was not ok');
     }
